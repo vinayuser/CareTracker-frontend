c0542cb343b336d056ea94c9e36f7ef724d4b10b
vinayuser <vinaybhardaj@gmail.com>
2026-06-24 22:02:58 +0530

    build changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://caretraker.com/api',
+  baseURL: 'https://caretraker.com/api',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
