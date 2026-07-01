31ef809859b6411a825bc9a42ab894db65f77c51
vinayuser <vinaybhardaj@gmail.com>
2026-07-01 08:54:36 +0530

    assesment

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://caretraker.com/api/api',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
